fb79f11fb2c33320306da3f83d6146839929ff6b
Kamil Jakóbczak <k_j1@vp.pl>
2025-09-02 11:10:34 +0200

    add base for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: '/recruitment-task-in-vue/',
 	plugins: [vue()],
 	test: {
 		environment: 'jsdom',
